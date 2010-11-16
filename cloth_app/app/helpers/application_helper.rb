c37183cd7fcde65d03a033a7e2de23fffb59b42f
Claudia Pfeil <cleo_sunshine@yahoo.de>
2010-11-16 17:13:39 +0100

    show notice messages, show selected user & package in order/package without blank

--- cloth_app/app/helpers/application_helper.rb
+++ cloth_app/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def get_notices
+    if params[:notice]
+     result = "<div class='notice'>"
+     params[:notice].each do |notice|
+          result = + notice.to_s
+     end
+    result = + "</div>"
+    result
+   end
+  end
 end
