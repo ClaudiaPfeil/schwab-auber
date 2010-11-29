50a9cca6701c03bb4ce9ca0280eb128eb0cc0738
Claudia Pfeil <cleo_sunshine@yahoo.de>
2010-11-29 11:50:24 +0100

    if there aren't any profiles, nothing to do for the helper

--- cloth_app/app/helpers/profiles_helper.rb
+++ cloth_app/app/helpers/profiles_helper.rb
@@ -50,19 +50,22 @@ module ProfilesHelper
   end
 
   def create_list_view(profile, name)
-    if name.to_s == "name"
-      profile.name
-    elsif name.to_s == "sex"
-      profile.sex == true ? "männlich" : "weiblich"
-    elsif !profile.option.first_letter_of_first_name.nil? && profile.option.first_letter_of_first_name == true && !(current_user.is? :admin)
-      profile.first_name.first + " " + profile.last_name
-    elsif !profile.option.first_letter_of_last_name.nil? && profile.option.first_letter_of_last_name == true && !(current_user.is? :admin)
-      profile.first_name + " " + profile.last_name.first
-    elsif name.to_s == "membership"
-      profile.membership == true ? I18n.t(:premium) : I18n.t(:base)
-    else 
-       profile.attributes[name.to_s]
+    unless profile.nil?
+      if name.to_s == "name"
+        profile.name
+      elsif name.to_s == "sex"
+        profile.sex == true ? "männlich" : "weiblich"
+      elsif profile.option.first_letter_of_first_name == true && !(current_user.is? :admin)
+        profile.first_name.first + " " + profile.last_name
+      elsif profile.option.first_letter_of_last_name == true && !(current_user.is? :admin)
+        profile.first_name + " " + profile.last_name.first
+      elsif name.to_s == "membership"
+        profile.membership == true ? I18n.t(:premium) : I18n.t(:base)
+      else
+         profile.attributes[name.to_s]
+      end
     end
+   
   end
   
 end
