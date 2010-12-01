e464f6a59a9ac504427d9772473da4e3e8d9b9fd
Claudia Pfeil <cleo_sunshine@yahoo.de>
2010-12-01 18:20:48 +0100

    Rechnungsanschrift aus Tabelle addresses auslesen

--- cloth_app/app/helpers/bills_helper.rb
+++ cloth_app/app/helpers/bills_helper.rb
@@ -3,5 +3,23 @@ module BillsHelper
   def formatted_date(date)
     date.strftime("%d.%m.%Y") unless date.nil?
   end
+
+  def get_address
+    addresses = @bill.user.addresses
+    if addresses && addresses.count > 1
+      addresses.each do |a|
+        if a.kind == true
+          return a
+        end
+      end
+    elsif addresses && addresses.count == 1
+      if addresses.kind == true
+       return addresses
+      end
+    else
+      nil
+    end
+    
+  end
   
 end
